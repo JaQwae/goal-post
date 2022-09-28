c9a025df15da3f1f6b1a0482c2c644c3d534798f
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-09-27 20:16:52 -0500

    add comment on seeds index.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,19 +1,23 @@
-// Seeds requires
+// Seeds requires 
+
+// ________Uncomment once models and seeds data created__________
+
+
 const sequelize = require('../config/connection');
 // const donationReceipentsData = require('./donationRecipientData');
 // const investors = require('./investors');
-const leagueData = require('./leagueData');
+// const leagueData = require('./leagueData');
 // const projectData = require('./projectData.json');
 
-const seedALL = async () => {
+// const seedALL = async () => {
 
-    await sequelize.sync({ force: true});
-    console.log('\n----- DATABASE SYNCED -----\n');
+//     await sequelize.sync({ force: true});
+//     console.log('\n----- DATABASE SYNCED -----\n');
 
-    await seedLeagues();
-    console.log('\n----- LEAGUE SEEDED -----\n');
+//     await seedLeagues();
+//     console.log('\n----- LEAGUE SEEDED -----\n');
 
-    process.exit(0)
-};
+//     process.exit(0)
+// };
 
-seedALL();
+// seedALL();
