5792095f6e5bd3317365df87c614957c0cc96d51
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-03 18:46:00 -0500

    delete donation routes

--- controllers/index.js
+++ controllers/index.js
@@ -3,17 +3,15 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const welcomeRoutes = require('./welcomeRoutes');
 const homeRoutes = require('./homeRoutes');
-const donationRoutes = require('./donationRoutes');
 const investorRoutes = require('./investorRoutes');
-const profileRoutes = require('./profileRoutes');
+// const profileRoutes = require('./profileRoutes');
 const loginRoutes = require('./loginRoutes');
 
 router.use('/', welcomeRoutes);
 router.use('/api', apiRoutes);
 router.use('/home', homeRoutes);
-router.use('/donation', donationRoutes);
 router.use('/investor', investorRoutes);
-router.use('/profile', profileRoutes);
+// router.use('/profile', profileRoutes);
 router.use('/login', loginRoutes);
 
 module.exports = router;
