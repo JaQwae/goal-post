8562c9fa27b9712969a5cfafd2c2ec60e3c9f3fd
Saul <nochochese2@gmail.com>
2022-10-03 19:22:10 -0500

    added singup/login database

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,4 +8,14 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// Login route
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
 module.exports = router;
