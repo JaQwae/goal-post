e1a58f886ad90dc37bf8fbda89112101e9e11ea4
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-03 19:43:51 -0500

    assosications estabhished

--- models/index.js
+++ models/index.js
@@ -3,8 +3,37 @@ const League = require('./League');
 const Investor = require('./Investor');
 const Donor = require('./Donor');
 const Recipient = require('./Recipient');
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: "CASCADE"
+});
 
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: "CASCADE"
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: "CASCADE"
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'Cascade'
+});
 
 League.hasMany(Donor, {
   foreignKey: 'league_id',
