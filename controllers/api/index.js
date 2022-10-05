c59b6be3cbb3fea44f993e3274818014c4f1b4bb
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-04 21:35:19 -0500

    dashboard post routes started

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
-// const postRoutes = require('./post-routes');
+const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 
 router.use('/user', userRoutes);
-// router.use('/post', postRoutes);
+router.use('/post', postRoutes);
 router.use('/comment', commentRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const myPostRoutes = require('./myPostRoutes');
 const postRoutes = require('./postRoutes');
 const leagueRoutes = require('./leagueRoutes');
 const loginRoutes = require('./loginRoutes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/mypost', myPostRoutes);
 router.use('/post', postRoutes);
 router.use('/league', leagueRoutes);
 router.use('/login', loginRoutes);
