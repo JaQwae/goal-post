cca41287dd53833314f2361bc03313d04a1ddf85
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-04 19:15:50 -0500

    got graphs to function

--- controllers/index.js
+++ controllers/index.js
@@ -3,14 +3,14 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const welcomeRoutes = require('./welcomeRoutes');
 const mmaPostRoutes = require('./mmaPostRoutes');
-const investorRoutes = require('./investorRoutes');
+const leagueRoutes = require('./leagueRoutes');
 // const profileRoutes = require('./profileRoutes');
 const loginRoutes = require('./loginRoutes');
 
 router.use('/', welcomeRoutes);
 router.use('/api', apiRoutes);
 router.use('/mma', mmaPostRoutes);
-router.use('/investor', investorRoutes);
+router.use('/league', leagueRoutes);
 // router.use('/profile', profileRoutes);
 router.use('/login', loginRoutes);
 
