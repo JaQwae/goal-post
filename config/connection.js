752ce9c61aa8e877c4e5cec9bf16cc31cc3db08a
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-10-06 20:51:20 -0500

    connection js fixed

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,27 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
+require('dotenv').config({ path: require('find-config')('.env') });
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: 'localhost',
+let sequelize
+
+if(process.env.JAWDB_URL) {
+    sequelize = new Sequelize(process.env.JAWDB_URL, {
+        host:'a3bm4rkhcnv0aoa6:gdvhi2sp31o5wi0w@au77784bkjx6ipju.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uovoke8ilsfkhnnj',
         dialect: 'mysql',
-        port: 3306,
-    }
-);
-
+        dialectOptions: {
+            decimalNumbers: true,
+        }
+    })
+}
+else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+        }
+    );
+}
 module.exports = sequelize;
